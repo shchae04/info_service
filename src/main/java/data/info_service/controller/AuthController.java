96cf2a6c1cf5086694691a2823a58af1f315d3fd
shchae04 <shchae04@b2bcni.co.kr>
2024-04-12 15:27:31 +0900

    EMAIL TOKEN 인증 (미완)

--- src/main/java/data/info_service/controller/AuthController.java
+++ src/main/java/data/info_service/controller/AuthController.java
@@ -1,15 +1,15 @@
 package data.info_service.controller;
 
-import data.info_service.entity.Member;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.InvalidPropertyException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/auth")
@@ -25,27 +25,41 @@ public class AuthController {
     }
 
     @GetMapping("/verify")
-    public Object verify(@RequestParam("token") String token, Member member) {
+    public Object verify(@RequestParam("token") String token) {
+
+        log.info("Verify Token: " + token);
+
         if (token == null) return "Invalid Token";
 
+        byte[] decodeBytes = Base64.getDecoder().decode(token);
+        String decodedToken = new String(decodeBytes);
+        String[] tokenParts = decodedToken.split(":");
 
-//        if (token.equalsIgnoreCase(member.getToken())) {
-        if (token.equalsIgnoreCase("2222")) {
-            // 이용 가능
-            log.info("Verify Success");
-            return "ACCEPT";
-
+        if (tokenParts.length != 2) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Token");
         }
 
-        return InvalidPropertyException.class;
+        String email = tokenParts[0];
+        Long tokenTime = Long.parseLong(tokenParts[1]);
+
+        long currentTime = System.currentTimeMillis();
+        long diff = currentTime - tokenTime;
+
+        if (diff > 1000L * 60 * 3) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Token Expired");
+        }
+
+        log.info("token 인증 성공");
+        return ResponseEntity.ok("Verify Success");
 
     }
 
     @PostMapping("/sendMail")
-//    public Object signup(String nickName, String email, String password, HttpSession session) throws Exception{
     public Object signup() throws Exception {
 
-        String token = UUID.randomUUID().toString();
+        String email = "shchae04@naver.com";
+
+        String token = createToken(email);
 
         log.info("token={}", token);
 
@@ -58,13 +72,21 @@ public class AuthController {
                 + "<h1> 안녕하세요. SH Corporation 입니다</h1>"
                 + "<br>"
                 + "<p>아래 링크를 클릭하면 이메일 인증이 완료됩니다.<p>"
-                + "<button><a href='http://localhost:8080/auth/verify?token=2222'>"
+                + "<button><a href='http://localhost:8080/auth/verify?token=" +token +
+                "'>"
                 + "인증</a></button>"
                 + "</div>";
         message.setText(body, "utf-8", "html");
         message.setFrom(new InternetAddress("shchae04@naver.com", "SYS_SH_CORP"));
         mailSender.send(message);
         return "OK";
+    }
+
+    public String createToken(String email) {
+        long currentTime = System.currentTimeMillis();
+        String token = email + ":" + currentTime;
+
+        return Base64.getEncoder().encodeToString(token.getBytes());
     }
 
 }
